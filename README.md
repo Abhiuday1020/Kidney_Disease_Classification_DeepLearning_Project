629e7b83809c90adacd03e1007517d964432c670
Abhiuday <abhiuday1020@gmail.com>
2024-09-09 21:48:29 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Kidney_Disease_Classification_DeepLearning_Project
+
+
+
+...
+## How to run?
+...
+### STEPS:
+
+Clone the Repository
+
+```bash
+https://github.com/Abhiuday1020/Kidney_Disease_Classification_DeepLearning_Project
+```
+
+...
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+conda create -n cnncls python=3.8 -y
+```
+
+```bash
+conda activate cnncls
+```
+
+...
+### STEP 02- install the requirements
+
+```base
+pip install -r requirements.txt
+```
--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
 
 project_name = 'cnnClassifier'
 
@@ -26,19 +26,21 @@ list_of_files = [
     "test.py"
 ]
 
-
 for filepath in list_of_files:
-    filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
+    try:
+        filepath = Path(filepath)
+        filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Created directory: {filedir} for the file: {filename}")
+        if filedir != "":
+            os.makedirs(filedir, exist_ok=True)
+            logging.info(f"Created directory: {filedir} for the file: {filename}")
 
-    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,"w") as f:
-            pass
-            logging.info(f"Created file: {filepath}")    
+        if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+            with open(filepath, "w") as f:
+                pass  
+            logging.info(f"Created file: {filepath}")
+        else:
+            logging.info(f"File: {filepath} already exists")
 
-    else:
-        logging.info(f"File: {filepath} already exists")        
+    except Exception as e:
+        logging.error(f"Error while processing file {filepath}: {str(e)}")
